6bc6b69965e4de144ab668226ce92aae80b395ef
Ashley <ASnell@student.neumont.edu>
2018-07-18 14:15:54 -0600

    7/18/18
    Class stuff

--- GameLib/NCGame/Engine/entity.cpp
+++ GameLib/NCGame/Engine/entity.cpp
@@ -1,15 +1,38 @@
 #include "entity.h"
+#include "renderComponent.h"
+
+
+void Entity::Update()
+{
+	for (Component* component : m_components)
+	{
+		component->Update();
+	}
+}
+
+void Entity::Draw()
+{
+	IRenderComponent* renderComponenet = GetComponenet<IRenderComponent>();
+	if (renderComponenet)
+	{
+		renderComponenet->Draw();
+	}
+}
 
 void Entity::AddComponent(Component * component)
 {
+	//assert(component);
+
+	m_components.push_back(component);
 }
 
 void Entity::RemoveComponent(Component * component)
 {
-	std::vector<Component*>::iterator iter = std::find(m_components.begin(), m_components.end(), component);
-	if (iter != m_components.end())
-	{
-		delete *iter;
-		m_components.erase(iter);
-	}
+	//assert(std::find(m_components.begin(), m_components.end(), component) != m_components.end());
+	
+	auto iter = std::find(m_components.begin(), m_components.end(), component);
+
+	delete *iter;
+	m_components.erase(iter);
+	
 }
--- GameLib/NCGame/Game/game.cpp
+++ GameLib/NCGame/Game/game.cpp
@@ -7,12 +7,14 @@
 #include "renderer.h"
 #include "textureManager.h"
 #include "timer.h"
+#include "entity.h"
+#include "transformComponent.h"
+#include "spriteComponent.h"
 
 Vector2D position(400.0f, 300.0f);
 float angle(0.0f);
-
 Text* text;
-
+Entity* entity = nullptr;
 
 bool Game::Initialize()
 {
@@ -25,6 +27,15 @@ bool Game::Initialize()
 	AudioSystem::Instance()->AddSound("horn", "..\\content\\horn.wav");
 
 	text = TextManager::Instance()->CreateText("Hello!", "..\\content\\Courier.ttf", 24, Color::red);
+
+	entity = new Entity(ID("player"));
+	TransformComponent* transformComponent = new TransformComponent(entity);
+	transformComponent->Create(Vector2D(30.0f, 30.0f));
+	entity->AddComponent(transformComponent);
+
+	SpriteComponent* spriteComponent = new SpriteComponent(entity);
+	spriteComponent->Create("..\\content\\car.bmp");
+	entity->AddComponent(spriteComponent);
 
 	m_running = success;
 
@@ -39,6 +50,9 @@ void Game::Shutdown()
 void Game::Update()
 {
 	m_running = !m_engine->IsQuit();
+	m_engine->Update();
+
+	entity->Update();
 
 	const Uint8* keystate = SDL_GetKeyboardState(nullptr);
 
@@ -66,11 +80,11 @@ void Game::Update()
 	SDL_Texture* texture = TextureManager::Instance()->GetTexture("..\\content\\car.bmp");
 	Renderer::Instance()->DrawTexture(texture, position, angle);
 
-	std::vector<Color> colors = { Color::red, Color::green, Color::white };
-	text->SetText("Hello World!", colors[rand() % colors.size()]);
-	text->Draw(Vector2D(10.0f, 10.0f), 0.0f);
+	entity->Draw();
 
 	Renderer::Instance()->EndFrame();
-
-	m_engine->Update();
 }
+
+//std::vector<Color> colors = { Color::red, Color::green, Color::white };
+//text->SetText("Hello World!", colors[rand() % colors.size()]);
+//text->Draw(Vector2D(10.0f, 10.0f), 0.0f);
